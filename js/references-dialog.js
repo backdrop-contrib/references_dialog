bf768d52c6dfbb0b7044f0797fb98230d66c8370
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2011-06-18 00:04:27 +0200

    Various improvements to our overlay handling

--- js/references-dialog.js
+++ js/references-dialog.js
@@ -23,17 +23,38 @@
   Drupal.ReferencesDialog.open = function(href) {
     if (!this.dialog_open) {
       href += "?render=references-dialog";
+      // Get the current window size and do 75% of the width and 90% of the height.
+      var window_width = $(window).width() / 100*75;
+      var window_height = $(window).height() / 100*90;
       this.open_dialog = $('<iframe class="references-dialog-iframe" src="' + href + '"></iframe>').dialog({
-        width: "auto",
-        height: "auto",
-        autoResize: true,
+        width: window_width,
+        height: window_height,
         modal: true,
         resizable: false,
-        position: "top",
+        position: ["center", 10],
         title: $(this).html(),
-        close: function() { this.dialog_open = false; }
-      }).width(700).height(600);
+        close: function() { Drupal.ReferencesDialog.dialog_open = false; }
+      }).width(window_width-10).height(window_height)
+      $(window).bind("resize scroll", function() {
+        // Move the dialog the main window moves.
+        if (typeof Drupal.ReferencesDialog == "object") {
+          Drupal.ReferencesDialog.open_dialog.
+            dialog("option", "position", ["center", 10]);
+          Drupal.ReferencesDialog.setDimensions();
+        }
+      });
       this.dialog_open = true;
+    }
+  }
+  
+  Drupal.ReferencesDialog.setDimensions = function() {
+    if (typeof Drupal.ReferencesDialog == "object") {
+      var window_width = $(window).width() / 100*75;
+      var window_height = $(window).height() / 100*90;
+      this.open_dialog.
+        dialog("option", "width", window_width).
+        dialog("option", "height", window_height).
+        width(window_width-10).height(window_height);
     }
   }
   Drupal.ReferencesDialog.close = function(entity_id, title) {
